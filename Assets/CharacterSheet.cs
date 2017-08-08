8ccdc028dd73b8d32e55d9020c7326f96c20f7da
John Hawley <jhawley@institute.local>
2017-08-08 18:55:41 -0500

    init commit

---
+++ Assets/CharacterSheet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterSheet", menuName = "Data/Character")]
+public class CharacterSheet : ScriptableObject {
+
+	public string displayName;
+	public string faceTexturePath;
+	private Texture2D _faceTexture;
+	public Texture2D faceTexture{
+		get {
+			if(_faceTexture == null){
+				_faceTexture = Resources.Load(faceTexturePath, typeof(Texture2D)) as Texture2D;
+			}
+			return _faceTexture;
+		}
+	}
+	
+	public void UnloadFaceTexture(){
+		Resources.UnloadAsset(_faceTexture);
+		_faceTexture = null;
+	}
+
+	public string description;
+	public int level = 1;
+
+
+	public AnimationCurve healthCurve = AnimationCurve.Linear(1, 10, 100, 9999);
+	public int health {
+		get {return Mathf.CeilToInt(healthCurve.Evaluate(level));
+		}
+	}
+
+	public AnimationCurve strengthCurve = AnimationCurve.Linear(1, 10, 100, 9999);
+	public int strength {
+		get {return Mathf.CeilToInt(strengthCurve.Evaluate(level));
+		}
+	}
+	public AnimationCurve speedCurve = AnimationCurve.Linear(1, 10, 100, 9999);
+	public int speed {
+		get {return Mathf.CeilToInt(speedCurve.Evaluate(level));
+		}
+	}
+}
---
+++ Assets/SaveManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class SaveManager : MonoBehaviour {
+
+	[System.Serializable]
+	public class SaveState{
+		public string sceneName;
+		public Vector3 playerPos;
+		public float playerHealth;
+	}
+
+	public static void Save(){
+		SaveState saveState = new SaveState();
+
+		saveState.sceneName = SceneManager.GetActiveScene().name;
+
+		saveState.playerPos = Vector3.zero;
+		saveState.playerHealth = 3;
+
+		string path = Path.Combine(Application.persistentDataPath, "save.json");
+		string json = JsonUtility.ToJson(saveState);
+
+		File.WriteAllText(path, json);
+	}
+
+	public static void Load(){
+		string path = Path.Combine(Application.persistentDataPath, "save.json");
+		string json = File.ReadAllText(path);
+		SaveState saveState = JsonUtility.FromJson<SaveState>(json);
+
+		SceneManager.LoadScene(saveState.sceneName);
+		Vector3 playerPos = saveState.playerPos;
+		float health = saveState.playerHealth;
+	}
+}
